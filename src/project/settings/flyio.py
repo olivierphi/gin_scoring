b284ed247f8a54cc3a45d5ec319f26ac47d92dc9
Olivier Philippon <olivier@dunsap.com>
2022-09-23 17:12:59 +0100

    [fly.io] Add management of the CSRF_TRUSTED_ORIGINS setting - doesn't work without it

--- src/project/settings/flyio.py
+++ src/project/settings/flyio.py
@@ -1,5 +1,8 @@
 from .production import *
 
+
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
+
 # We let Fly.io make sure that we're using HTTPS, and disable these settings as the Docker container won't
 # have enough knowledge of the HTTP layer to be able to check these things:
 SECURE_SSL_REDIRECT = False
